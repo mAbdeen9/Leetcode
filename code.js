9f9b062f3c05b10f9457b5d56acd86f9871e7d06
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-10-03 22:12:04 +0300

    update

--- code.js
+++ code.js
@@ -143,3 +143,15 @@ const isPalindrome = function (x) {
 };
 
 // -----
+// Given a sorted array of distinct integers and a target value, return the index if the target is found.
+// If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+const searchInsert = function (arr, target) {
+  if (arr.includes(target)) return arr.indexOf(target);
+  if (arr[arr.length - 1] < target) return arr.length;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= target) return i;
+  }
+};
+// [1,3,5,6]
+console.log(searchInsert([1, 3, 5, 6], 3));

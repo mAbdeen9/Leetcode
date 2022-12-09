bf1738716ab3a65c3d8d6f7b411ba0733b503a24
mAbdeen9 <93052095+mAbdeen9@users.noreply.github.com>
2022-12-09 12:27:36 +0200

    AOL

--- code.js
+++ code.js
@@ -507,9 +507,24 @@ var addBinary = function (a, b) {
   return resultAsBinary.split("").reverse().join("");
 };
 
-console.log(
-  addBinary(
-    "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
-    "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
-  )
-);
+// console.log(
+//   addBinary(
+//     "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+//     "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
+//   )
+// );
+
+var reverse = function (x) {
+  let revNumber;
+  if (x.toString().includes("-")) {
+    revNumber = `-${x.toString().split("-")[1].split("").reverse().join("")}`;
+  } else {
+    revNumber = x.toString().split("").reverse().join("");
+  }
+
+  return revNumber > Math.pow(-2, 31) && revNumber < Math.pow(2, 31) - 1
+    ? revNumber
+    : 0;
+};
+
+// console.log(reverse(-123));
